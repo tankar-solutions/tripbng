a49f6afda8a25ea4e5b8447a3f950948259c9ea2
Hetuk <hetuk2212@gmail.com>
2025-01-29 12:45:25 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    productionSourceMaps: false,
+    webpack(config, { isServer }) {
+      if (!isServer) {
+        config.parallelism = 2; // Set max number of workers for build
+      }
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
